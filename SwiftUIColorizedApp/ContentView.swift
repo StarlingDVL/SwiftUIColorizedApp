a2dfcb388d84ef1ea4e1862606e7c8acbbd0d7d1
ISkvor <starlingy608@gmail.com>
2022-04-25 02:44:03 +0300

    setup keyboard

--- SwiftUIColorizedApp/ContentView.swift
+++ SwiftUIColorizedApp/ContentView.swift
@@ -11,16 +11,27 @@ struct ContentView: View {
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    @FocusState private var textFieldIsEditing: Bool
 
     var body: some View {
         ZStack {
             Color.yellow.ignoresSafeArea()
-            VStack {
+            VStack(spacing: 30) {
                 ColorizedRectangleView(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue)
                 VStack {
                     SliderView(sliderValue: $redSliderValue, color: .red)
                     SliderView(sliderValue: $greenSliderValue, color: .green)
                     SliderView(sliderValue: $blueSliderValue, color: .blue)
+                }
+                .focused($textFieldIsEditing)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            textFieldIsEditing = false
+                        }
+                    }
                 }
                 Spacer()
             }
