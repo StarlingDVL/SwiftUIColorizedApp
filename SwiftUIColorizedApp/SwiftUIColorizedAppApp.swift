06af9453a25b1b3689c7e49922333b34810c6093
ISkvor <starlingy608@gmail.com>
2022-04-24 20:59:38 +0300

    Initial Commit

---
+++ SwiftUIColorizedApp/SwiftUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedAppApp.swift
+//  SwiftUIColorizedApp
+//
+//  Created by Юрий Скворцов on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
