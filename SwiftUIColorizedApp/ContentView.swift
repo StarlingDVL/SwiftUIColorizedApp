c4efe2232d734eafdac38d9f0d302821fef4da56
ISkvor <starlingy608@gmail.com>
2022-04-24 21:31:57 +0300

    First step

--- SwiftUIColorizedApp/ContentView.swift
+++ SwiftUIColorizedApp/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = 100.0
+    @State private var sliderTextValue = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color.yellow.ignoresSafeArea()
+            VStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 350, height: 200)
+                HStack(spacing: 10) {
+                    Text("\(lround(sliderValue))")
+                        .frame(width: 50)
+                    Slider(value: $sliderValue, in:  0...255)
+                    TextField("255", text: $sliderTextValue)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 50)
+                }
+                Spacer()
+            }
             .padding()
+        }
     }
 }
 
